8448b807e17e69e63e399f83089c664f14cd8a06
halfgingerbeard <121182955+halfgingerbeard@users.noreply.github.com>
2022-12-21 15:13:17 -0500

    Add files via upload

---
+++ Day16.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 15:03:20 2022
+
+@author: joshua.joy
+"""
+
+import streamlit as st
+
+st.title('Customizing the theme of Streamlit apps')
+
+st.write('Contents of the `.streamlit/config.toml` file of this app')
+
+st.code("""
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
+""")
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
